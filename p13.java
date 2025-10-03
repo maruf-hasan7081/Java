34aec501fca3f88189ecb3b86168580b00ae66a8
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2025-10-03 19:24:53 +0600

    Add files via upload

---
+++ p13.java
@@ -0,0 +1,25 @@
+public class p13 {
+ 
+
+    public static void main(String[] args) {
+        
+        int marks= 80;
+
+        if (marks >= 80){
+            System.out.println("Grade:A+");
+        }
+        else if  (marks >=70){
+            System.out.println("Grade:B+");
+        }
+        else if (marks >= 60){
+            System.out.println("Grade:c+");
+        }
+        else if (marks >= 50){
+            System.out.println("Grade:A+");
+        }
+        else{
+            System.out.println("Grade:Fail");
+        }
+    }
+    
+}
