3618750f6e8ea396d87a4267b758d0cd1ed41b17
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2025-10-11 14:22:55 +0600

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class WhileLoop {
+	
+	public static void main(String[] args){
+		
+		Scanner sca=new Scanner(System.in);
+		System.out.print("Enter integer number:");
+		int i=sca.nextInt();
+		
+		//int i=0;
+		while(i<=10){
+			System.out.println(i);
+		i++;}
+	}
+	
+    
+}
