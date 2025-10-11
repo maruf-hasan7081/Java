c41b31fbd06bda0e7bd6a3a16adcd9c78008449e
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2025-10-11 16:01:47 +0600

    Add files via upload

---
+++ p18.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class p18{
+	public static void main(String[] args){
+		Scanner sca=new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int n = sca.nextInt();
+		int sum=0;
+		
+		for(int i=0; i<n; i++){
+			sum += i;
+		}
+		System.out.println("sum is: "+sum);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+		
