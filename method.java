6f144f01434535998aca53126f4183acefe4f4e7
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2025-10-19 22:33:00 +0600

    Add files via upload

---
+++ method.java
@@ -0,0 +1,17 @@
+public class method {
+
+    int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args)
+
+    {
+        method obj=new method();
+        int sum=obj.add(5,10);
+        System.out.println("sum is : "+sum);
+       
+    }
+
+
+}
