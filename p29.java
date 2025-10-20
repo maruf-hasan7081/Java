9da4d3e2b815971fce85457d928d4bdfac8a98fa
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2025-10-20 23:20:54 +0600

    Add files via upload

---
+++ p29.java
@@ -0,0 +1,21 @@
+//Write a method that takes two numbers and prints their difference.
+
+
+public class p29 {
+	
+	
+	int difference(int a, int b){
+		return a-b;
+	}
+
+    public static void main(String[] args) {
+		p29 a= new p29();
+		int Tdifference=a.difference(5,7);
+		System.out.println(" their difference: "+Tdifference);
+		
+		
+		
+        
+    }
+    
+}
